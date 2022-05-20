51d8ada4ad2e214433419ec3567d5a5e47f483b5
Ruben <[connor.j.schulz@gmail.com]>
2022-05-20 04:44:23 -0400

    finished inquirer questions and writing file permissions

--- app.js
+++ app.js
@@ -1,2 +1,180 @@
-const fs = require('fs');
 const inquirer = require('inquirer');
+const generatePage = require('./src/create-page');
+
+
+//questions to ask the user
+const initialQuestions = [
+    {
+        type: 'input',
+        name: 'managerName',
+        message: 'Enter team managers name.'
+    },
+    {
+        type: 'input',
+        name: 'managerID',
+        message: 'Enter team managers ID.'
+    },
+    {
+        type: 'input',
+        name: 'managerEmail',
+        message: 'Enter team managers email address.'
+    },
+    {
+        type: 'input',
+        name: 'managerOffice',
+        message: 'Enter team managers office number.'
+    },
+    {
+        type: 'list',
+        name: 'addMember',
+        message: 'Which member would you like to add?',
+        choices: [
+            'Engineer',
+            'Intern',
+            "I'm done building my team."
+        ]
+    }
+]
+
+const loopingQuestions = [
+    {
+        type: 'list',
+        name: 'addMember',
+        message: 'Which member would you like to add?',
+        choices: [
+            'Engineer',
+            'Intern',
+            "I'm done building my team."
+        ]
+    },
+    
+]
+
+const engineerQuestions = [
+    {
+        type: 'input',
+        name: 'engineerName',
+        message: 'Enter engineers name.'
+    },
+    {
+        type: 'input',
+        name: 'engineerID',
+        message: 'Enter engineers ID.'
+    },
+    {
+        type: 'input',
+        name: 'engineerEmail',
+        message: 'Enter engineers email address.'
+    },
+    {
+        type: 'input',
+        name: 'engineerGithub',
+        message: 'Enter engineers github address.'
+    }
+]
+
+const internQuestions = [
+    {
+        type: 'input',
+        name: 'internName',
+        message: 'Enter interns name.'
+    },
+    {
+        type: 'input',
+        name: 'internID',
+        message: 'Enter interns ID.'
+    },
+    {
+        type: 'input',
+        name: 'internEmail',
+        message: 'Enter interns email address.'
+    },
+    {
+        type: 'input',
+        name: 'internSchool',
+        message: 'Enter interns school.'
+    }
+]
+
+let manager = {};
+
+let engineers = []
+let interns = [];
+
+async function promptManager(){
+    let response = '';
+    await inquirer.prompt(initialQuestions)
+    .then((answers) => {
+        manager.name = answers.managerName;
+        manager.id = answers.managerID;
+        manager.email = answers.managerEmail;
+        manager.officeNumber = answers.managerOffice;
+        response = answers.addMember;
+    });
+
+    return response;
+};
+
+async function promptEngineer(){
+    let currentEngineer = {};
+    await inquirer.prompt(engineerQuestions)
+    .then((answers) => {
+        currentEngineer.name = answers.engineerName;
+        currentEngineer.id = answers.engineerID;
+        currentEngineer.email = answers.engineerEmail;
+        currentEngineer.github = answers.engineerGithub;
+        engineers.push(currentEngineer);
+    })
+
+    await inquirer.prompt(loopingQuestions)
+    .then((loop) => {
+        if(loop.addMember === "I'm done building my team."){
+            generatePage(manager, engineers, interns);
+            return;
+        } 
+        else if(loop.addMember === "Engineer") {
+            promptEngineer();
+        }
+        else if(loop.addMember === "Intern"){
+            promptIntern();
+        }
+    })
+};
+
+async function promptIntern(){
+    let currentIntern = {};
+    await inquirer.prompt(internQuestions)
+    .then((answers) => {
+        currentIntern.name = answers.internName;
+        currentIntern.id = answers.internID;
+        currentIntern.email = answers.internEmail;
+        currentIntern.school = answers.internSchool;
+        interns.push(currentIntern);
+    })
+
+    await inquirer.prompt(loopingQuestions)
+    .then((loop) => {
+        if(loop.addMember === "I'm done building my team."){
+            generatePage(manager, engineers, interns);
+            return;
+        } 
+        else if(loop.addMember === "Engineer") {
+            promptEngineer();
+        }
+        else if(loop.addMember === "Intern"){
+            promptIntern();
+        }
+    })
+};
+
+
+promptManager()
+.then((answer) => {
+    if(answer === 'Engineer'){
+        promptEngineer();
+    }
+    if(answer === 'Intern'){
+        promptIntern();
+    }
+})
+
---
+++ src/create-page.js
@@ -0,0 +1,96 @@
+const { writeFile, copyFile } = require('./generate-site');
+
+const generatePage = (manager, engineers, interns) => {
+    const content = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>team roster</title>
+</head>
+<body>
+    <header>
+        <h1>My Team</h1>
+    </header>
+    <body>
+        <section class="cards">
+            ${generateCards(manager, engineers, interns)}
+        </section>
+    </body>
+</body>
+</html>
+    `
+
+    writeFile(content)
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+}
+
+const generateCards = (manager, engineers, interns) => {
+    let cards = `
+<div class="card">
+    <div class="top-card">
+        <h2>${manager.name}</h2>
+        <h3>Manager</h3>    
+    </div>
+    <div class="bottom-card">
+        <div class="bottom-card-content">
+            <p>ID: ${manager.id}</p>
+            <p>Email: ${manager.email}</p>
+            <p>Office Number: ${manager.officeNumber}</p>    
+        </div>
+    </div>
+</div>
+`;
+
+    engineers.forEach(engineer => {
+        cards += `
+        <div class="card">
+            <div class="top-card">
+                <h2>${engineer.name}</h2>
+                <h3>Engineer</h3>    
+            </div>
+            <div class="bottom-card">
+                <div class="bottom-card-content">
+                    <p>ID: ${engineer.id}</p>
+                    <p>Email: ${engineer.email}</p>
+                    <p>Github: ${engineer.github}</p>    
+                </div>
+            </div>
+        </div>
+        `
+    })
+
+    interns.forEach(intern => {
+        cards += `
+        <div class="card">
+            <div class="top-card">
+                <h2>${intern.name}</h2>
+                <h3>Intern</h3>    
+            </div>
+            <div class="bottom-card">
+                <div class="bottom-card-content">
+                    <p>ID: ${intern.id}</p>
+                    <p>Email: ${intern.email}</p>
+                    <p>School: ${intern.school}</p>    
+                </div>
+            </div>
+        </div>
+        `
+    })
+
+    return cards;
+}
+
+module.exports = generatePage;
